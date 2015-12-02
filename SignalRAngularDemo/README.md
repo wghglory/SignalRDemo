ff0312361af0451e7c5e444cb17dcf5500221909
Guanghui Wang <wghglory89@gmail.com>
2015-12-02 11:28:41 -0800

    signalr angular

---
+++ SignalRAngularDemo/README.md
@@ -0,0 +1,10 @@
+SignalRDemo with AngularJS
+==========================
+
+SignalR 2.0 with ASP.NET MVC 5, AngularJS and Web API Demo
+
+See blog post: http://squarewidget.com/signalr-mvc-angular-web-api
+
+
+
+
---
+++ SignalRAngularDemo/SignalRAngularDemo/app/services/widgetSvc.js
@@ -0,0 +1,69 @@
+﻿
+// The widget service is a function responsible for bootstrapping the SignalR 
+// widgets hub proxy. It exposes the sendRequest() function to invoke the sendWidget
+// dynamic method on the hub.
+//
+// This service was written by Ravi Kiran and documented on his blog at
+// http://sravi-kiran.blogspot.com/2013/09/ABetterWayOfUsingAspNetSignalRWithAngularJs.html
+
+
+//service: return a instance 
+//app.service('widgetSvc', function ($, $rootScope) {
+//    var proxy = null;
+
+//    var initialize = function () {
+
+//        // fetch connection object and create proxy
+//        var connection = $.hubConnection();
+//        this.proxy = connection.createHubProxy('widgets');
+
+//        // start connection
+//        connection.start();
+
+//        this.proxy.on('newWidget', function (data) {
+//            $rootScope.$emit("newWidget", data);
+//        });
+//    };
+
+//    var sendRequest = function () {
+//        // invoke sendWidget method defined in hub
+//        this.proxy.invoke('sendWidget');
+//    };
+
+//    return {
+//        initialize: initialize,
+//        sendRequest: sendRequest
+//    };
+//});
+
+
+
+
+//factory: return a object with properties.
+app.factory('widgetSvc', function ($, $rootScope) {
+
+    var service = {};
+
+    var proxy = null;
+
+    service.initialize = function () {
+
+        // fetch connection object and create proxy
+        var connection = $.hubConnection();
+        this.proxy = connection.createHubProxy('widgets');
+
+        // start connection
+        connection.start();
+
+        this.proxy.on('newWidget', function (data) {
+            $rootScope.$emit("newWidget", data);
+        });
+    };
+
+    service.sendRequest = function () {
+        // invoke sendWidget method defined in hub
+        this.proxy.invoke('sendWidget');
+    };
+
+    return service;
+});
